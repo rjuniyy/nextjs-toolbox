5aabd0b8b490536b6b4a4ab3ed0e3f1a0c00d247
rjuniyy <juniyanasr@gmail.com>
2023-09-19 01:18:53 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,20 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Reset Password</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Reset Password" />
         <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
+        <p className="description">Buat Password Baru dibawah ini.</p>
         <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
